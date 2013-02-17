98a47671aa3a676cfb317c4b26047a13a9e1766d
AntonLovmar <antonlovmar@gmail.com>
2013-02-17 16:09:14 +0100

    Static values for colors

---
+++ src/Tetris/Components/ColorCodes.java
@@ -0,0 +1,13 @@
+package Tetris.Components;
+
+public class ColorCodes {
+	
+	public static final int RED = 0;
+	public static final int ORANGE = 1;
+	public static final int YELLOW = 2;
+	public static final int GREEN = 3;
+	public static final int TEAL = 4;
+	public static final int BLUE = 5;
+	public static final int PURPLE = 6;
+
+}
