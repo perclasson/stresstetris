22d10531c08e91126dd18eb8900384828c750128
AntonLovmar <antonlovmar@gmail.com>
2013-02-17 13:39:32 +0100

    durr

---
+++ src/BTDeviceFinder.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.DiscoveryListener;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.ServiceRecord;
+
+
+public class BTDeviceFinder {
+
+	private ArrayList<RemoteDevice> devicesDiscovered = new ArrayList<RemoteDevice>();
+
+	public static void main(String[] args) {
+		BTDeviceFinder finder = new BTDeviceFinder();
+		finder.startSearch();
+	}
+	
+	public void startSearch() {
+		try {
+			final Object inquiryCompletedEvent = new Object();
+			DiscoveryListener listener = new DiscoveryListener() {
+
+				public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+					System.out.println("Device " + btDevice.getBluetoothAddress()
+							+ " found");
+					devicesDiscovered.add(btDevice);
+					try {
+						System.out.println("     name "
+								+ btDevice.getFriendlyName(false));
+						System.out.println("     address "
+								+ btDevice.getBluetoothAddress());
+					} catch (IOException cantGetDeviceName) {
+					}
+				}
+
+				public void inquiryCompleted(int discType) {
+					System.out.println("Device Inquiry completed!");
+					synchronized (inquiryCompletedEvent) {
+						inquiryCompletedEvent.notifyAll();
+					}
+				}
+
+				public void serviceSearchCompleted(int transID, int respCode) {
+				}
+
+				public void servicesDiscovered(int transID,
+						ServiceRecord[] servRecord) {
+				}
+			};
+			
+			
+			synchronized (inquiryCompletedEvent) {
+				boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent()
+						.startInquiry(DiscoveryAgent.GIAC, listener);
+				if (started) {
+					System.out.println("wait for device inquiry to complete...");
+					try {
+						inquiryCompletedEvent.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println(devicesDiscovered.size()
+							+ " device(s) found");
+				}
+			}
+		} catch (BluetoothStateException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
---
+++ src/SPPConnector.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+import javax.bluetooth.*;
+import javax.microedition.io.*;
+
+/**
+ * Class that implements an SPP Server which accepts single line of message from
+ * an SPP client and sends a single line of response to the client.
+ */
+public class SPPConnector {
+
+	// start server
+	private void startServer() throws IOException {
+
+		// Create a UUID for SPP
+		UUID uuid = new UUID("1101", true);
+		// Create the servicve url
+		String connectionString = "btspp://101DC0869F1E:3";
+		// open server url
+		StreamConnection streamConn= (StreamConnection) Connector
+				.open(connectionString);
+
+		// Wait for client connection
+		/*System.out
+				.println("\nServer Started. Waiting for clients to connect...");
+		StreamConnection connection = streamConnNotifier.acceptAndOpen();*/
+
+		RemoteDevice dev = RemoteDevice.getRemoteDevice(streamConn);
+		System.out.println("Remote device address: "
+				+ dev.getBluetoothAddress());
+		System.out.println("Remote device name: " + dev.getFriendlyName(true));
+
+		// read string from spp client
+		boolean listening = true;
+		InputStream inStream = streamConn.openInputStream();
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(
+				inStream));
+		while (listening) {
+			String lineRead = bReader.readLine();
+			if (lineRead == null) {
+				listening = false;
+			} else {
+				System.out.println(lineRead);
+			}
+		}
+	
+		streamConn.close();
+
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		// display local device address and name
+		LocalDevice localDevice = LocalDevice.getLocalDevice();
+		System.out.println("Address: " + localDevice.getBluetoothAddress());
+		System.out.println("Name: " + localDevice.getFriendlyName());
+
+		SPPConnector connector = new SPPConnector();
+		connector.startServer();
+
+	}
+}
