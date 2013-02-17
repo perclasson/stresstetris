66671689fd41c91c9327d4202e8055f54201cf5f
AntonLovmar <antonlovmar@gmail.com>
2013-02-17 14:22:11 +0100

    Dummy class was added

--- src/Bluetooth/Dummy.java
+++ src/Bluetooth/Dummy.java
@@ -1,6 +1,48 @@
 package Bluetooth;
 
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Dummy {
+	private DataInputStream input;
+	private FileInputStream fis;
+
+	public Dummy(String filePath) {
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(filePath);
+			input = new DataInputStream(fis);
+		} catch (FileNotFoundException e) {
+			System.err.println("The file \"" + filePath + "\" could not be found.");
+		}
+	}
 	
+	public Dummy() {
+		String file = "log.txt";
+		try {
+			fis = new FileInputStream(file);
+			input = new DataInputStream(fis);
+		} catch (FileNotFoundException e) {
+			System.err.println("The file \"" + file + "\" could not be found.");
+		}
+		
+	}
 	
-}
+	public InputStream openInputStream() {
+		return input;
+	}
+	
+	public void shutDown() {
+		try {
+			input.close();
+			fis.close();
+		} catch (IOException e) {
+			System.err.println("Error while closing the inputstreams.");
+		}
+	}
+	
+ }
+ 
