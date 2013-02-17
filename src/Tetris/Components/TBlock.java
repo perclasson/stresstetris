5c8287323d84e7311489f3f60479b09f659a8d85
AntonLovmar <antonlovmar@gmail.com>
2013-02-17 18:12:50 +0100

    New TBlock

--- src/Tetris/Components/TBlock.java
+++ src/Tetris/Components/TBlock.java
@@ -15,8 +15,7 @@ public class TBlock extends Block {
 
 	public ArrayList<Square> generateSquares(float x, float y) {
 		squares.add(new Square(color, x, y));
-		mainSquare = new Square(color, x, y + Codes.BLOCK_SIZE);
-		squares.add(mainSquare);
+		squares.add(new Square(color, x, y + Codes.BLOCK_SIZE)); //main square
 		squares.add(new Square(color, x + Codes.BLOCK_SIZE, y
 				+ Codes.BLOCK_SIZE));
 		squares.add(new Square(color, x - Codes.BLOCK_SIZE, y
@@ -25,23 +24,39 @@ public class TBlock extends Block {
 		return squares;
 	}
 	public void rotate() {
-		editPosition(getPosition()+1 % 4);
+		editPosition((getPosition()+1) % 4);
 	}
 	
 	public void reverseRotate() {
-		editPosition(getPosition()-1 % 4);
+		editPosition((getPosition()+3) % 4);
 	}
 	
 	private void editPosition(int i) {
-		switch (i) {
-			case 0:
-				break;
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-		}
+		float x = squares.get(1).getX();
+		float y = squares.get(1).getY();
+		System.out.println(i);
+	    switch(i) {
+		    case 0:
+				squares.get(0).setY(y).setX(x- Codes.BLOCK_SIZE);
+			    squares.get(2).setY(y- Codes.BLOCK_SIZE).setX(x);
+			    squares.get(3).setY(y).setX(x+ Codes.BLOCK_SIZE);
+			  break;
+		    case 1:
+				squares.get(0).setY(y- Codes.BLOCK_SIZE).setX(x);
+			    squares.get(2).setY(y).setX(x+ Codes.BLOCK_SIZE);
+			    squares.get(3).setY(y+ Codes.BLOCK_SIZE).setX(x);
+			  break;
+		    case 2:
+				squares.get(0).setY(y).setX(x- Codes.BLOCK_SIZE);
+			    squares.get(2).setY(y).setX(x+ Codes.BLOCK_SIZE);
+			    squares.get(3).setY(y+Codes.BLOCK_SIZE).setX(x);
+		   	 break;
+		    case 3:
+				squares.get(0).setY(y).setX(x-Codes.BLOCK_SIZE);
+			    squares.get(2).setY(y+Codes.BLOCK_SIZE).setX(x);
+			    squares.get(3).setY(y-Codes.BLOCK_SIZE).setX(x);
+			 break;
+	    }
+	    setPosition(i);
 	}
 }
