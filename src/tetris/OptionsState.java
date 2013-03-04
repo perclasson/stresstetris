8b591ae8032a0d0d3478686e31e5ccde73a13661
Per Classon <perwclasson@gmail.com>
2013-03-04 16:29:54 +0100

    OptionsStage

--- src/tetris/OptionsState.java
+++ src/tetris/OptionsState.java
@@ -2,6 +2,9 @@ package tetris;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.io.File;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -15,16 +18,12 @@ import org.newdawn.slick.state.StateBasedGame;
 
 public class OptionsState extends BasicGameState {
 	private int stateID = 2;
-	Image background = null;
-	Image startGameOption = null;
-	Image exitOption = null;
-	int menuX;
-	int menuY;
-	Sound theme;
-	UnicodeFont font;
-	float startGameScale = 1;
-	float exitScale = 1;
-	FontButton[] buttons;
+	private Image background = null;
+	private UnicodeFont font, smallFont;
+	private FontButton[] buttons;
+	private File[] files;
+	private FontButton optionsButton;
+	public File optionsFile;
 
 	public OptionsState(int stateID) throws SlickException {
 		this.stateID = stateID;
@@ -40,30 +39,53 @@ public class OptionsState extends BasicGameState {
 		font.addNeheGlyphs();
 		font.loadGlyphs();
 
-		buttons = new FontButton[2];
-		buttons[0] = new FontButton(container, font, "STATISTICS", 260, 310, game,
+		smallFont = new UnicodeFont(new java.awt.Font("Verdana", Font.BOLD, 20));
+		smallFont.getEffects().add(new ColorEffect(new Color(211, 211, 211)));
+		smallFont.addNeheGlyphs();
+		smallFont.loadGlyphs();
+
+		File folder = new File("conf/");
+		files = folder.listFiles();
+
+		buttons = new FontButton[files.length + 1];
+		buttons[0] = new FontButton(container, font, "BACK", 200, 270, game,
 				stateID) {
-			@Override
-			public void perform() {
-				System.out.println("LAWL");
-			}
-		};
-		
-		buttons[1] = new FontButton(container, font, "BACK", 330, 370,
-				game, stateID) {
 			@Override
 			public void perform() {
 				game.enterState(Game.MAINMENUSTATE);
 			}
 		};
 
+		for (int i = 0; i < files.length; i++) {
+			final File file = files[i];
+
+			buttons[i + 1] = new FontButton(container, smallFont,
+					file.getName(), 200, 345 + i * 30, game, stateID) {
+				@Override
+				public void perform() {
+					setIsEnabled(false);
+					optionsButton.setIsEnabled(true);
+					optionsFile = file;
+					optionsButton = this;
+				}
+			};
+
+			if (file == optionsFile) {
+				buttons[i + 1].setIsEnabled(false);
+			}
+		}
+
+		// Sets the first file as the chosen button.
+		optionsFile = files[0];
+		optionsButton = buttons[1];
+		optionsButton.setIsEnabled(false);
 	}
 
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
 		background.draw(0, 0);
-		
+
 		for (FontButton button : buttons) {
 			button.render(container, g);
 		}
