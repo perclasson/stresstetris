53c60cfcc6ea7b89e783c0c0865a525b9d3481f7
AntonLovmar <antonlovmar@gmail.com>
2013-03-20 14:22:11 +0100

    Updated ChartDrawer: it now plots the gsr

--- src/tetris/ChartDrawer.java
+++ src/tetris/ChartDrawer.java
@@ -11,6 +11,8 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
+import bluetooth.EDAReader;
+
 public class ChartDrawer extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -23,25 +25,31 @@ public class ChartDrawer extends JFrame {
 	public ChartDrawer(DifficultyManager manager) {
 		super("Difficulty chart");
 		// This will create the dataset
-		CategoryDataset dataset = createDataset();
+		CategoryDataset dataset = createDiffDataset();
 		// based on the dataset we create the chart
-		JFreeChart chart = createChart(dataset, "Difficulty over time");
+		JFreeChart chart = createDiffChart(dataset, "Difficulty over time");
 		// we put the chart into a panel
 		ChartPanel chartPanel = new ChartPanel(chart);
 		// default size
 		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
 		// add it to our application
+		CategoryDataset dataset2 = createEDADataset();
+
+		JFreeChart chart2 = createEDAChart(dataset2, "EDA over time");
+		// we put the chart into a panel
+		ChartPanel chartPanel2 = new ChartPanel(chart2);
+		// default size
+		chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+		// add it to our application
 		setContentPane(chartPanel);
-		
-
-
+		setContentPane(chartPanel2);
 	}
 
 	/**
 	 * Creates a sample dataset
 	 */
 
-	private CategoryDataset createDataset() {
+	private CategoryDataset createDiffDataset() {
 		DefaultCategoryDataset result = new DefaultCategoryDataset();
 		final String series1 = "Session";
 		ArrayList<Double> timeStamps = DifficultyManager.getTimeStamps();
@@ -55,12 +63,44 @@ public class ChartDrawer extends JFrame {
 		return result;
 
 	}
+	
+	private CategoryDataset createEDADataset() {
+		DefaultCategoryDataset result = new DefaultCategoryDataset();
+		final String series1 = "Session";
+		ArrayList<Long> timeStamps =EDAReader.getTimeStampsGSR();
+		ArrayList<Float> gsrStamps = EDAReader.getGSRStamps();
+		DecimalFormat ds = new DecimalFormat("#.");
+		for(int i = 0; i < timeStamps.size(); i++) {
+			Long time = Long.valueOf(ds.format(timeStamps.get(i)));
+			result.addValue(gsrStamps.get(i),series1,time);
+		}
+	
+		return result;
+
+	}
 
 	/**
 	 * Creates a chart
 	 */
 
-	private JFreeChart createChart(CategoryDataset dataset, String title) {
+	private JFreeChart createEDAChart(CategoryDataset dataset, String title) {
+
+		final JFreeChart chart = ChartFactory.createLineChart(
+				"Difficulty over time", // chart title
+				"Time (seconds)", // domain axis label
+				"Difficulty", // range axis label
+				dataset, // data
+				PlotOrientation.VERTICAL, // orientation
+				true, // include legend
+				true, // tooltips
+				false // urls
+				);
+		
+		return chart;
+
+	}
+	
+	private JFreeChart createDiffChart(CategoryDataset dataset, String title) {
 
 		final JFreeChart chart = ChartFactory.createLineChart(
 				"Difficulty over time", // chart title
