23c853755c02fbd9ce1db68ab46242e4e8928e00
AntonLovmar <antonlovmar@gmail.com>
2013-03-20 19:07:29 +0100

    Added curve fitting

--- src/tetris/ChartDrawer.java
+++ src/tetris/ChartDrawer.java
@@ -64,11 +64,53 @@ public class ChartDrawer extends JFrame {
 
 	}
 	
+	private ArrayList<Float> fitCurve(ArrayList<Float> edaStamps, ArrayList<Long> timeStamps) {
+		ArrayList<Float> fitted = new ArrayList<Float>();
+		for(int i = 0; i < edaStamps.size(); i+=4) {
+			long x1 = timeStamps.get(i);
+			long x2 = timeStamps.get(i+1);
+			long x3 = timeStamps.get(i+2);
+			long x4 = timeStamps.get(i+3);
+			
+			float y1 = edaStamps.get(i);
+			float y2 = edaStamps.get(i+1);
+			float y3 = edaStamps.get(i+2);
+			float y4 = edaStamps.get(i+3);
+			
+			float c1 = 2*(y1+y2+y3+y4); //konstanter som står framför de okända i normalekvationerna
+			float c2 = 2*(x1*y1+x2*y2+x3*y3+x4*y4);
+			float a1 = 2*4;
+			float a2 = 2*(x1+x2+x3+x4);
+			float b1 = 2*(x1+x2+x3+x4);
+			float b2 = 2*(x1*x1+x2*x2+x3*x3+x4*x4);
+			
+			float Det11 = c1*b2-b1*c2; //Vi räknar ut determinanterna för att lösa systemet
+		    float Det12 = a1*b2-b1*a2;
+
+			float Det21 = a1*c2-a2*c1;
+			float Det22 = a1*b2-a2*b1;
+			
+			float b1Sol = Det11/Det12; //ySol och xSol bildar ekvationen --y=xSol + ySol*x--
+			float b2Sol = Det21/Det22;
+			fitted.add(b1Sol+b2Sol*y4);
+		}
+		return fitted;
+	}
+	
+	public ArrayList<Long> formatStamps(ArrayList<Long> timeStamps) {
+		ArrayList<Long> result = new ArrayList<Long>();
+		for(int i = 0; i < timeStamps.size();i+=4) {
+			result.add(timeStamps.get(i));
+		}
+		return result;
+	}
+	
 	private CategoryDataset createEDADataset() {
 		DefaultCategoryDataset result = new DefaultCategoryDataset();
 		final String series1 = "Session";
 		ArrayList<Long> timeStamps =EDAReader.getTimeStampsGSR();
-		ArrayList<Float> gsrStamps = EDAReader.getGSRStamps();
+		ArrayList<Float> gsrStamps = fitCurve(EDAReader.getGSRStamps(), timeStamps);
+		timeStamps = formatStamps(timeStamps);
 		for(int i = 0; i < timeStamps.size(); i++) {
 			Long time = timeStamps.get(i);
 			result.addValue(gsrStamps.get(i),series1,time);
