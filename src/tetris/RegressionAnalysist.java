d8a0cf705cc97e903d8faaaad33f668cd601d5c4
AntonLovmar <antonlovmar@gmail.com>
2013-03-27 21:15:08 +0100

    Added a class for correlation analysis (also included unit testing)

---
+++ src/tetris/RegressionAnalysist.java
@@ -0,0 +1,33 @@
+package tetris;
+
+import java.util.List;
+
+public class RegressionAnalysist {
+
+	public double correlationAnalysis(List<Double> xValues, List<Double> yValues) {
+		if(xValues.size() != yValues.size()) {
+			return -2.0;
+		}
+		double xMean = 0;
+		double yMean = 0;
+		double r = 0;
+		for(int i = 0; i < xValues.size(); i++) {
+			xMean += xValues.get(i);
+			yMean += yValues.get(i);
+		}
+		yMean = yMean/yValues.size();
+		xMean = xMean/xValues.size();
+
+		double corrSum = 0;
+		double xSum = 0;
+		double ySum = 0;
+		for(int i = 0; i< xValues.size(); i++) {
+			corrSum += (xValues.get(i)-xMean)*(yValues.get(i)-yMean);
+			xSum += Math.pow((xValues.get(i)-xMean),2);
+			ySum += Math.pow((yValues.get(i)-yMean),2);
+		}
+
+		r = corrSum/(Math.sqrt(xSum)*Math.sqrt(ySum));
+		return r;
+	}
+}
